25a993d47be1f9594c681273f17f439a42bc2c49
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2020-03-09 01:12:37 +0100

    Add makeTags

---
+++ src/article/tags.js
@@ -0,0 +1,9 @@
+export default function makeTags(input) {
+  if (Array.isArray(input))
+    return input
+  if (typeof input === 'string')
+    return input.split(', ')
+
+  return []
+}
+
---
+++ src/article/tags.test.js
@@ -0,0 +1,23 @@
+import makeTags from './tags'
+
+describe('makeTags', () => {
+  it('does not modify input when an array', () => {
+    const expected = ['aap', 'noot', 'mies']
+    const result = makeTags(expected)
+
+    expect(result).toBe(expected)
+  })
+  it('splits the input into an arry when a string', () => {
+
+    const expected = ['aap', 'noot', 'mies']
+    const result = makeTags('aap, noot, mies')
+
+    expect(result).toEqual(expected)
+  })
+  it('returns an empty array on other input', () => {
+    expect(makeTags()).toEqual([])
+    expect(makeTags({})).toEqual([])
+    expect(makeTags(12)).toEqual([]) // eslint-disable-line
+
+  })
+})
